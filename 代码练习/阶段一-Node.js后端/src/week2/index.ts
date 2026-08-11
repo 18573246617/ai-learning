a56887819ecf74558331bb2f06209ce778742d96
李清泉 <18573246617@163.com>
2026-08-11 14:03:14 +0800

    文件夹目录修改

--- 代码练习/阶段一-Node.js后端/src/week2/index.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "./app.js"
-
-const PORT = Number(process.env.PORT ?? 3000)
-
-const app = createApp()
-
-app.listen(PORT, () => {
-  console.log(`服务已启动: http://localhost:${PORT}`)
-})
